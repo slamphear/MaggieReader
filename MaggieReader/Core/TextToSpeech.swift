883f99c00b47cf1e66667f4256a2e27a413632db
Steven Lamphear <stevenlamphear@mac.com>
2024-06-16 16:25:16 -0500

    Use Combine when processing audio

--- MaggieReader/Core/TextToSpeech.swift
+++ MaggieReader/Core/TextToSpeech.swift
@@ -5,9 +5,187 @@
 //  Created by Steven Lamphear on 5/27/24.
 //
 
+import Foundation
 import AVFoundation
-import SwiftKeychainWrapper
+import Combine
 import OpenAI
+
+func convertTextToSpeech(text: String, voice: AudioSpeechQuery.AudioSpeechVoice, completion: @escaping (URL?) -> Void) {
+    print("Starting convertTextToSpeech")
+    let chunks = chunkText(text, chunkSize: 4096)
+    print("Text split into \(chunks.count) chunks")
+
+    let chunkProcessingPublishers = chunks.enumerated().map { index, chunk -> AnyPublisher<(Int, URL?), Never> in
+        return processChunk(chunk: chunk, voice: voice)
+            .map { (index, $0) }
+            .eraseToAnyPublisher()
+    }
+
+    Publishers.MergeMany(chunkProcessingPublishers)
+        .collect()
+        .flatMap { indexedURLs -> AnyPublisher<URL?, Never> in
+            let sortedURLs = indexedURLs.sorted(by: { $0.0 < $1.0 }).compactMap { $0.1 }
+            return joinAudioFiles(urls: sortedURLs)
+                .eraseToAnyPublisher()
+        }
+        .sink { finalURL in
+            print("Completed with final URL: \(String(describing: finalURL))")
+            completion(finalURL)
+        }
+        .store(in: &cancellables)
+}
+
+var cancellables = Set<AnyCancellable>()
+
+func processChunk(chunk: String, voice: AudioSpeechQuery.AudioSpeechVoice) -> AnyPublisher<URL?, Never> {
+    return Future<URL?, Never> { promise in
+        print("Processing chunk: \(chunk.prefix(50))...")
+        textToSpeechAPI(text: chunk, voice: voice) { url in
+            print("Chunk processed with URL: \(String(describing: url))")
+            promise(.success(url))
+        }
+    }.eraseToAnyPublisher()
+}
+
+func textToSpeechAPI(text: String, voice: AudioSpeechQuery.AudioSpeechVoice, completion: @escaping (URL?) -> Void) {
+    print("Calling textToSpeechAPI")
+    guard let openAI = getOpenAIClient() else {
+        print("Failed to get OpenAI client")
+        completion(nil)
+        return
+    }
+
+    let query = AudioSpeechQuery(model: .tts_1_hd, input: text, voice: voice, responseFormat: .aac, speed: 1.0)
+
+    openAI.audioCreateSpeech(query: query) { result in
+        switch result {
+        case .success(let audio):
+            let tempAACURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("aac")
+            do {
+                print("Audio data size: \(audio.audio.count) bytes")
+                try audio.audio.write(to: tempAACURL)
+                print("Audio written to \(tempAACURL)")
+
+                // Verify if the file can be opened as an AVAsset
+                let asset = AVURLAsset(url: tempAACURL)
+                asset.loadValuesAsynchronously(forKeys: ["tracks", "duration"]) {
+                    var error: NSError?
+                    let tracksStatus = asset.statusOfValue(forKey: "tracks", error: &error)
+                    let durationStatus = asset.statusOfValue(forKey: "duration", error: &error)
+                    if tracksStatus == .loaded && durationStatus == .loaded {
+                        print("Successfully verified audio file at \(tempAACURL)")
+                        completion(tempAACURL)
+                    } else {
+                        print("Failed to verify audio file at \(tempAACURL): \(String(describing: error))")
+                        completion(nil)
+                    }
+                }
+            } catch {
+                print("Error writing audio: \(error)")
+                completion(nil)
+            }
+        case .failure(let error):
+            print("API call failed with error: \(error)")
+            completion(nil)
+        }
+    }
+}
+
+func joinAudioFiles(urls: [URL]) -> AnyPublisher<URL?, Never> {
+    return Future<URL?, Never> { promise in
+        print("Joining audio files")
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let outputURL = documentDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("m4a")
+
+        let composition = AVMutableComposition()
+        let loadAssetPublisher = urls.publisher.flatMap { url -> AnyPublisher<(AVAsset, [AVAssetTrack], CMTime), Error> in
+            let asset = AVURLAsset(url: url)
+            let tracksPublisher = asset.loadTracksPublisher(withMediaType: .audio)
+            let durationPublisher = asset.loadDurationPublisher()
+
+            return Publishers.Zip3(Just(asset).setFailureType(to: Error.self), tracksPublisher, durationPublisher)
+                .eraseToAnyPublisher()
+        }
+
+        loadAssetPublisher
+            .collect()
+            .sink(receiveCompletion: { completion in
+                print("Finished loading assets with completion: \(completion)")
+                if case let .failure(error) = completion {
+                    print("Failed with error: \(error)")
+                    promise(.success(nil))
+                }
+            }, receiveValue: { assetTracksAndDurations in
+                print("Received asset tracks and durations")
+                for (asset, tracks, duration) in assetTracksAndDurations {
+                    if let assetTrack = tracks.first {
+                        let compositionTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+                        try? compositionTrack?.insertTimeRange(CMTimeRangeMake(start: .zero, duration: duration), of: assetTrack, at: composition.duration)
+                    }
+                }
+
+                let exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A)!
+                exporter.outputFileType = .m4a
+                exporter.outputURL = outputURL
+
+                exporter.exportAsynchronously {
+                    switch exporter.status {
+                    case .completed:
+                        print("Export completed: \(outputURL)")
+                        promise(.success(outputURL))
+                    default:
+                        print("Export failed with status: \(exporter.status)")
+                        promise(.success(nil))
+                    }
+                }
+            })
+            .store(in: &cancellables)
+    }.eraseToAnyPublisher()
+}
+
+extension AVAsset {
+    func loadTracksPublisher(withMediaType mediaType: AVMediaType) -> AnyPublisher<[AVAssetTrack], Error> {
+        return Future<[AVAssetTrack], Error> { promise in
+            print("Loading tracks for media type: \(mediaType)")
+            self.loadTracks(withMediaType: mediaType) { tracks, error in
+                if let tracks = tracks {
+                    print("Loaded tracks: \(tracks.count)")
+                    promise(.success(tracks))
+                } else if let error = error {
+                    print("Error loading tracks: \(error)")
+                    promise(.failure(error))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+
+    func loadDurationPublisher() -> AnyPublisher<CMTime, Error> {
+        return Future<CMTime, Error> { promise in
+            print("Loading duration")
+            Task {
+                do {
+                    let duration: CMTime = try await self.load(.duration)
+                    print("Loaded duration: \(duration)")
+                    promise(.success(duration))
+                } catch {
+                    print("Error loading duration: \(error)")
+                    promise(.failure(error))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+}
+
+// Debugging function to inspect file headers
+func printFileHeader(at url: URL) {
+    do {
+        let data = try Data(contentsOf: url)
+        let header = data.prefix(32) // Read the first 32 bytes
+        print("File header: \(header.map { String(format: "%02x", $0) }.joined(separator: " "))")
+    } catch {
+        print("Failed to read file header: \(error)")
+    }
+}
 
 func chunkText(_ text: String, chunkSize: Int) -> [String] {
     var chunks: [String] = []
@@ -20,112 +198,23 @@ func chunkText(_ text: String, chunkSize: Int) -> [String] {
         if endIndex < text.endIndex {
             if let periodIndex = text[..<endIndex].lastIndex(of: ".") {
                 endIndex = text.index(after: periodIndex)
+            } else if let spaceIndex = text[..<endIndex].lastIndex(of: " ") {
+                endIndex = spaceIndex
             }
         }
 
         let chunk = String(text[startIndex..<endIndex]).trimmingCharacters(in: .whitespacesAndNewlines)
         chunks.append(chunk)
+        print("\nCreated chunk (\(chunk.count) characters): \(chunk)")
 
         // Check if we've reached the end of the text
         if endIndex == text.endIndex {
             break
         }
 
+        // Move startIndex to the character after the last character used to avoid overlapping
         startIndex = endIndex
     }
 
     return chunks
-}
-
-func convertTextToSpeech(text: String, voice: AudioSpeechQuery.AudioSpeechVoice, completion: @escaping (URL?) -> Void) {
-    let chunkSize = 4000  // or any appropriate chunk size
-    let textChunks = chunkText(text, chunkSize: chunkSize)
-    let uniqueID = UUID().uuidString
-    let tempDirectory = FileManager.default.temporaryDirectory
-    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-    var audioURLs: [(Int, URL)] = []  // Tuple to store chunk index and URL
-    let dispatchGroup = DispatchGroup()
-
-    guard let openAI = getOpenAIClient() else {
-        completion(nil)
-        return
-    }
-
-    for (index, chunk) in textChunks.enumerated() {
-        dispatchGroup.enter()
-        let query = AudioSpeechQuery(model: .tts_1_hd, input: chunk, voice: voice, responseFormat: .aac, speed: 1.0)
-        openAI.audioCreateSpeech(query: query) { result in
-            switch result {
-            case .success(let audio):
-                let tempURL = tempDirectory.appendingPathComponent("\(uniqueID)_chunk_\(index).aac")
-                do {
-                    try audio.audio.write(to: tempURL)
-                    audioURLs.append((index, tempURL))
-                    print("Chunk \(index) length: \(audio.audio.count) bytes")
-                } catch {
-                    print("Error writing audio to file: \(error)")
-                }
-            case .failure(let error):
-                print("Error creating speech: \(error)")
-            }
-            dispatchGroup.leave()
-        }
-    }
-
-    dispatchGroup.notify(queue: .main) {
-        // Sort audioURLs by chunk index to ensure they are in the correct order
-        audioURLs.sort { $0.0 < $1.0 }
-        let sortedURLs = audioURLs.map { $0.1 }
-        let outputURL = documentsDirectory.appendingPathComponent("\(uniqueID).m4a")
-        stitchAudioFiles(audioURLs: sortedURLs, outputURL: outputURL) { success in
-            if success {
-                for (_, url) in audioURLs {
-                    try? FileManager.default.removeItem(at: url)
-                }
-                completion(outputURL)
-            } else {
-                print("Error stitching audio files")
-                completion(nil)
-            }
-        }
-    }
-}
-
-func stitchAudioFiles(audioURLs: [URL], outputURL: URL, completion: @escaping (Bool) -> Void) {
-    let composition = AVMutableComposition()
-    let track = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)
-
-    var insertTime = CMTime.zero
-
-    for (index, url) in audioURLs.enumerated() {
-        let asset = AVAsset(url: url)
-        guard let assetTrack = asset.tracks(withMediaType: .audio).first else { continue }
-        let timeRange = CMTimeRange(start: .zero, duration: asset.duration)
-        do {
-            try track?.insertTimeRange(timeRange, of: assetTrack, at: insertTime)
-            insertTime = CMTimeAdd(insertTime, asset.duration)
-            print("Inserted chunk \(index) at time \(insertTime.seconds)")
-        } catch {
-            print("Error inserting time range: \(error)")
-            completion(false)
-            return
-        }
-    }
-
-    guard let exportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A) else {
-        completion(false)
-        return
-    }
-
-    exportSession.outputURL = outputURL
-    exportSession.outputFileType = .m4a
-    exportSession.exportAsynchronously {
-        if exportSession.status == .completed {
-            print("Stitched audio file successfully")
-            completion(true)
-        } else {
-            print("Error exporting stitched audio file: \(exportSession.error?.localizedDescription ?? "unknown error")")
-            completion(false)
-        }
-    }
 }
